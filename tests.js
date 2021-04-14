fd8946d75d3aef3467bca4e87d171a8f5617084c
lonnen <chris.lonnen@gmail.com>
2021-04-13 20:22:35 -0700

    1 asynch mocha test suite, please
    the testrunner will need to provide their own API key and be on the network. I want to know if this works actually works, not whether I can mock a thing that makes it work. Likewise I want to know when flickr changes something and this starts falling over.

---
+++ tests.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const FlickrSDK = require('flickr-sdk');
+
+const { fetchRandomPhotoURL } = require('.');
+
+// eslint-disable-next-line no-underscore-dangle
+
+describe('the non-corsica business logic', () => {
+  it('requires the testrunner to provide a flickr_api_key', () => {
+    assert.ok(process.env.flickr_api_key);
+  });
+
+  it('should return a random photo url from a photoset', () => {
+    const flickrApiKey = process.env.flickr_api_key;
+    const flickr = new FlickrSDK(flickrApiKey);
+
+    // corsica image test set https://www.flickr.com/photos/153716660@N03/sets/72157718978213760
+    const userID = '153716660@N03';
+    const photosetID = '72157718978213760';
+
+    return fetchRandomPhotoURL(flickr, userID, photosetID)
+      .then((url) => {
+        switch (url) {
+          case 'https://live.staticflickr.com/65535/51114161883_0756bdc5ea_o.jpg':
+            assert.ok('known image 1');
+            break;
+          case 'https://live.staticflickr.com/65535/51113684872_122f9a4981_o.jpg':
+            assert.ok('known image 2');
+            break;
+          default:
+            assert.fail('Flickr API returned a value that did not match the known test set urls');
+            break;
+        }
+      });
+  });
+});
